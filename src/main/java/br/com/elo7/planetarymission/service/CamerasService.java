d4e576b1882af78ca0d5c106a54750972cc4dc57
Joao Lopes <joao.lopes@sinapsisenergia.com>
2019-02-25 22:43:49 -0300

    jersey libs updated
    jerseys lib updated to latest version (28) and refactoring of cameras services to work with the new libs

--- src/main/java/br/com/elo7/planetarymission/service/CamerasService.java
+++ src/main/java/br/com/elo7/planetarymission/service/CamerasService.java
@@ -2,18 +2,19 @@ package br.com.elo7.planetarymission.service;
 
 import java.util.Queue;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 
 import br.com.elo7.planetarymission.exceptions.SerializePhotosException;
 import br.com.elo7.planetarymission.service.response.Photos;
 
 public abstract class CamerasService {
 
-	protected Client client = Client.create();
-	protected WebResource resource;
-	protected ClientResponse response;
+	protected Client client = ClientBuilder.newClient();
+	protected WebTarget webTarget;
+	protected Response response;
 	
 	public abstract Queue<Photos> getPhotosQueryByMartianRotation(int martianRotation) throws SerializePhotosException;
 	
